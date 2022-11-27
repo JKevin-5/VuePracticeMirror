ebf85687cae4baa0ca96541b9df9bbba237da6cd
Jkevin <15810880126@163.com>
2022-11-28 00:14:39 +0800

    提交部分修改

--- Html/typewriter-js/typewriter.js
+++ Html/typewriter-js/typewriter.js
@@ -9,12 +9,19 @@ h1.innerHTML = h1.textContent
 let delay = 0;
 document.querySelectorAll("span").forEach((span, index) => {
   delay += 0.1;
-  if (index === 6) delay += 0.2;
-  var br = document.createElement("br");
-  debugger;
-  if (index === 7) span.append(br);
+  if (index === 4) delay += 0.3;
+  if (index === 6) delay += 0.3;
+  if (index === 8) delay += 0.3;
   span.style.setProperty("--delay", `${delay}s`);
 });
+var brMethod = function (num) {
+  var br = document.createElement("br");
+  h1.insertBefore(br, document.querySelectorAll("span")[num]);
+};
+brMethod(4);
+brMethod(6);
+brMethod(8);
+brMethod(10);
 
 h1.addEventListener("animationend", (e) => {
   if (e.target === document.querySelector("h1 span:last-child")) {

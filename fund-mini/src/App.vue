b3382286ed66bdba6b910e942d2caf61d6f70c74
XMALPHA\mid2098 <VP01108@globalymc.com>
2023-01-10 17:08:49 +0800

    新增fund-mini项目vue-cli3 vue2.0

---
+++ fund-mini/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+      <router-view/>
+  </div>
+</template>
+<style scoped>
+#app{
+  margin: 0; padding: 0;height:100%;
+}
+</style>
---
+++ fund-mini/src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nut-row>
+            <nut-col :span="12">
+                <div class="flex-content">span:12sss</div>
+            </nut-col>
+            <nut-col :span="12">
+                <div class="flex-content flex-content-light">span:12</div>
+            </nut-col>
+        </nut-row>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>

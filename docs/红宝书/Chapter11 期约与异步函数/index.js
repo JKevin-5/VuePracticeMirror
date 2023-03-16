41f0395832121c3c2ac4170c0fb855f02f80999d
Jkevin <15810880126@163.com>
2023-03-16 17:34:29 +0800

    对期约的学习

--- docs/红宝书/Chapter11 期约与异步函数/index.js
+++ docs/红宝书/Chapter11 期约与异步函数/index.js
@@ -1,31 +1,54 @@
 const test1 = function(){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            console.log("1");
-            resolve("1");
-        },2000);
+            console.log("业务1成功回调");
+            resolve("业务1返回的内容");
+        },1000);
     })
 }
-const test2 = function(){
-    test1().then(res=>{
-        return new Promise((resolve,reject)=>{
-            setTimeout(()=>{
-                console.log("2");
-                resolve("2");
-            },1000);
-        })
-    })
-}
-test2();
 
-
-const test3 = function(){
+const test2 = function(res){
     return new Promise((resolve,reject)=>{
-        Vue.http.post(
-            url,
-            formData)
-        .then(res=>{
-            resolve(res);
-        });
+        setTimeout(()=>{
+            console.log(res+"+业务2成功回调");
+            resolve("业务2返回的内容");
+        },1000);
     })
 }
+
+const test3 = function(res){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(res+"+业务3成功回调");
+            resolve("业务3返回的内容");
+        },1000);
+    })
+}
+
+// test1().then(res=>{
+//     test2(res);
+// }).then(res=>{
+//     console.log(res)
+//     return test3(res)
+// }).then(res=>{
+//     console.log("延迟")
+// }).catch(err=>{
+//     console.log(err)
+// })
+
+// console.log(typeof test1)
+
+const test4 = function(res){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            // console.log(res+"+业务3成功回调");
+            reject("业务3返回的内容");
+        },1000);
+    }).then((res)=>{
+        console.log("success")
+    },(err)=>{
+        console.log("error")
+    })
+}
+
+test4()

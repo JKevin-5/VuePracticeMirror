52625e74e3d4c0d26d77b5d30a922a16942a6837
JKevin <15810880126@163.com>
2023-04-22 23:50:10 +0800

    新增对脚手架的学习

---
+++ docs/Vue2/vue_basic/19_vue脚手架/vue-index/02_src_ref/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1 v-text="msg" ref="title"></h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <button @click="showDom">点我获取真实DOM</button>
+    <HelloWorld ref="hello" id="hello"></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      msg: 'Hello world!',
+    };
+  },
+  methods: {
+    showDom() {
+      console.log(this.$refs.title) //z'
+      console.log(this.$refs.hello)
+      console.log(document.getElementById('hello'))
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ docs/Vue2/vue_basic/19_vue脚手架/vue-index/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1 v-text="msg" ref="title"></h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <button @click="showDom">点我获取真实DOM</button>
+    <Student name="Jom" sex="man" :age="18"/>
+  </div>
+</template>
+
+<script>
+import Student from './components/Student.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Student
+  },
+  data() {
+    return {
+      msg: 'Hello world!',
+    };
+  },
+  methods: {
+    showDom() {
+      console.log(this.$refs.title) //z'
+      console.log(this.$refs.hello)
+      console.log(document.getElementById('hello'))
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
